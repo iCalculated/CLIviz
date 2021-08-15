737231c69b1b6e3e657ecffeffa3396055389366
iCalculated <fufa0001@gmail.com>
2021-08-15 12:26:15 -0500

    module investigation

--- viz.py
+++ viz.py
@@ -1,11 +1,23 @@
 from sorts.bubble import bubble_sort
+import importlib
 import time
-from heartrate import trace, files
 import time
+import sys
 
 if __name__ == "__main__":
-    trace(files=files.path_contains("sorts"), browser=False)
+    import sys
+
+    """
     unsorted = list(range(100,0,-1))
+
     start = time.process_time()
     print(*bubble_sort(unsorted), sep=",")
     print(f"Processing time: {time.process_time() - start}")
+    """
+
+    lib = input("File to get sort from: ")
+    sort = importlib.import_module("sorts." + lib)
+
+    modulenames = set(sys.modules) & set(globals())
+    allmodules = [sys.modules[name] for name in modulenames]
+    print(allmodules)

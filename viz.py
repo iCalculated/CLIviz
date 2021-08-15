c92aa63741216a6a8bd24f09603607b31f3d626c
iCalculated <fufa0001@gmail.com>
2021-08-15 12:35:16 -0500

    Find sort in module

--- viz.py
+++ viz.py
@@ -4,20 +4,23 @@ import time
 import time
 import sys
 
-if __name__ == "__main__":
-    import sys
+def get_functions(module):
+    return [f for f in dir(module) if not f.startswith("__")]
 
-    """
+def find_sort(module):
+    funcs = get_functions(module)
+    if len(funcs) != 1:
+        raise SortModuleError("Too many functions in sort file! (Should be 1)")
+    return getattr(module, funcs[0])
+
+if __name__ == "__main__":
+
+    lib = input("File to get sort from: ")
+    sort_module = importlib.import_module("sorts." + lib)
+    sort = find_sort(sort_module)
+    
     unsorted = list(range(100,0,-1))
 
     start = time.process_time()
-    print(*bubble_sort(unsorted), sep=",")
+    print(sort(unsorted), sep=",")
     print(f"Processing time: {time.process_time() - start}")
-    """
-
-    lib = input("File to get sort from: ")
-    sort = importlib.import_module("sorts." + lib)
-
-    modulenames = set(sys.modules) & set(globals())
-    allmodules = [sys.modules[name] for name in modulenames]
-    print(allmodules)

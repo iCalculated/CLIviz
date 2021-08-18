72928cdddb349c247eb287095f31ee68e93756e6
iCalculated <fufa0001@gmail.com>
2021-08-18 17:40:18 -0500

    restructued

--- viz.py
+++ viz.py
@@ -1,88 +1,22 @@
-import importlib
-import time
-import sys
-import os
-import cmd
-from nubia import Nubia, Options, command, context, argument, PluginInterface, eventbus
-from nubia.internal.cmdbase import AutoCommand
-from termcolor import cprint
+import ast
+from loguru import logger
+logger.add("out.log", backtrace=True, diagnose=True)  #
 
-def get_functions(module):
-    return [f for f in dir(module) if not f.startswith("__")]
+source_code = """
+def bubble_sort(collection):
+    length = len(collection)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                swapped = True
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+        if not swapped:
+            break  
+    return collection
+"""
+ast.parse(source, mode="exec")
 
-@command("test")
-def test():
-    "Lets me mess around with Nubia"
-    ctx = context.get_context()
-    print(ctx.sort)
-    print(ctx.sort_name)
-
-@command("sort")
-def sort():
-    "Visualizes the selected sort"
-    ctx = context.get_context()
-
-    to_sort = ctx.unsorted[:]
-    cprint(f"{to_sort} -> {ctx.sort(to_sort)}", "cyan")
-    cprint(f"\tvia {ctx.sort_name}", "yellow")
-
-@command("find_sort")
-@argument("module_name", description="the module to get a sort from", positional=True)
-def find_sort(module_name: str):
-    "Finds a sort function in a given module"
-    ctx = context.get_context()
-
-    module = importlib.import_module("sorts." + module_name)
-    funcs = get_functions(module)
-    if len(funcs) != 1:
-        raise SortModuleError("Too many functions in sort file! (Should be 1)")
-    ctx.sort = getattr(module, funcs[0])
-    ctx.sort_name = module_name
-
-
-class NubiaFunctionContext(context.Context):
-    def __init__(self):
-        self.sort = None
-        self.sort_name = None
-        self.unsorted = list(range(10,0,-1))
-        super().__init__()
-
-    def on_connected(self, *args, **kwargs):
-        pass
-
-    def on_cli(self, cmd, args):
-        self.funcs = {}
-        # dispatch the on connected message
-        self.verbose = args.verbose
-        self.registry.dispatch_message(eventbus.Message.CONNECTED)
-
-    def on_interactive(self, args):
-        self.verbose = args.verbose
-        ret = self._registry.find_command("connect").run_cli(args)
-        if ret:
-            raise exceptions.CommandError("Failed starting interactive mode")
-        # dispatch the on connected message
-        self.registry.dispatch_message(eventbus.Message.CONNECTED)
-
-class Plugin(PluginInterface):
-    def create_context(self):
-        return NubiaFunctionContext()
-
-    def get_commands(self):
-        return [
-            AutoCommand(test),
-            AutoCommand(find_sort),
-            AutoCommand(sort),
-        ]
-
-if __name__ == "__main__":
-
-    cli = cmd.Cmd()
-    plugin = Plugin()
-    shell = Nubia(
-            name="CLIviz",
-            plugin=plugin,
-            options=Options(persistent_history=False, 
-                auto_execute_single_suggestions=True)
-            )
-    sys.exit(shell.run())
+explorer = ASTExplorer(source_code)
+for result in explorer.getVariables():
+    print(f"Found variable '{result.var}' with a value of '{result.expression}' (type: '{result.vType.__name__}')")

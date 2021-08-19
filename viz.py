d74effec59e91e7b1e627520286ba5aad13e44fc
iCalculated <fufa0001@gmail.com>
2021-08-19 13:41:41 -0500

    AST exploration

--- viz.py
+++ viz.py
@@ -10,5 +10,20 @@ def bubble_sort(collection):
     print(length)
     return collection
 """
-tree = ast.dump(ast.parse(source_code, mode="exec"), indent=2)
-print(tree)
+tree = ast.parse(source_code, mode="exec")
+with open("tree.ast","w") as f:
+    f.write(ast.dump(tree, indent=4))
+
+print()
+
+def get_fields(node):
+    return [f for f in dir(node) if not f.startswith("__")]
+
+for node in ast.walk(tree):
+    if isinstance(node, ast.FunctionDef):
+        print(f"{get_fields(node)=}")
+        print(node.name)
+        print(get_fields(node.args))
+        print(get_fields(node.args.args))
+        print(get_fields(node.args.args[0]))
+        print(node.args.args[0].arg)
